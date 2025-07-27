13b9ee72983e6b8d1224836f79abec4157a04c71
Muaaz Shaikh <muaaz5731@gmail.com>
2025-07-27 15:52:49 +0530

    Added the doc analyser and kept a placeholder design for classes present

---
+++ src/docanalyser/data_analysis.py
@@ -0,0 +1,20 @@
+import os
+import sys
+from utils.model_loader import ModelLoader
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from model.models import *
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
+
+class DocumentAnalyzer:
+    """
+    Analyzes Docs using a pretrained model and logs all actions and supports session based organization
+    """
+    def __init__(self):
+        pass
+
+    def analyze_document(self):
+        pass
+
+    
