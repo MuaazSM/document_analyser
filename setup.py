85d39863cfdf5d70b17990209c1f8fc1770988c6
Muaaz Shaikh <muaaz5731@gmail.com>
2025-07-27 14:38:33 +0530

    Added config

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="document_analyser",
+    version="0.1",
+    author="Muaaz Shaikh",
+    author_email="muaaz5731@gmail.com",  
+    description="Document Portal Project",
+    packages=find_packages(),
+)
