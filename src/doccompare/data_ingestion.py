3e259c8be12a552ae4dc110165b676d83ab81a7e
Muaaz Shaikh <muaaz5731@gmail.com>
2025-08-10 21:38:06 +0530

    Updated the data ingestions file for doc comparator

--- src/doccompare/data_ingestion.py
+++ src/doccompare/data_ingestion.py
@@ -5,16 +5,47 @@ from logger.custom_logger import CustomLogger
 from exception.custom_exception import DocumentPortalException
 
 class DocumentIngestion:
-    def __init__(self):
-        pass
+    def __init__(self, base_dir):
+        self.log = CustomLogger().get_logger(__name__)
+        self.base_dir = Path(base_dir)
+        self.base_dir.mkdir(parents=True, exist_ok=True)
 
     def delete_existing_file(self):
-        pass
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error deleting existing file: {e}")
+            raise DocumentPortalException("")
 
     def save_uploaded_file(self):
-        pass
+        """
+        Saves uploaded files to a specific directory.
+        """
+        try:
+            pass
+        except Exception as e:
+            self.log.error(f"Error uploading file: {e}")
+            raise DocumentPortalException("An error occured while saving the uploaded files", sys)
     
-    def read_pdf(self):
-        pass
+    def read_pdf(self, pdf_path: Path) -> str:
+        """
+        Reads a PDF file and extracts text from each page.
+        """
+        try:
+            with fitz.open(self,pdf_path) as doc:
+                if doc.is_encrypted:
+                    raise ValueError(f"PDF is encrypted: {pdf_path.name}")
+                all_text = []
+                for page_num in range(doc.page_count):
+                    page = doc.load_page(page_num)
+                    text = page.get_text() 
+                    if text.strip():
+                        all_text.append(f"\n --- Page {page_num + 1} --- \n{text}")
+                    self.log.info("PDF read successfully", file = str(pdf_path), pages = len(all_text))
+                    return "\n".join(all_text)
+
+        except Exception as e:
+            self.log.error(f"Error reading PDF: {e}")
+            raise DocumentPortalException("An error encountered while reading the PDF", sys)
 
 
