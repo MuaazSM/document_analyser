6d93e7bb7f7590690839754e00c20cb147ec5871
Muaaz Shaikh <muaaz5731@gmail.com>
2025-09-04 01:27:24 +0530

    Edited the document comparison code

--- src/doccompare/document_comparator.py
+++ src/doccompare/document_comparator.py
@@ -1,14 +1,13 @@
 import sys
-import pandas as pd 
-from logger.custom_logger import CustomLogger
-from dotenv import load_dotenv              
-from exception.custom_exception import DocumentPortalException
-from model.models import *
-from prompt.prompt_lib import PROMPT_REGISTRY
+from dotenv import load_dotenv
+import pandas as pd
+from langchain_core.output_parsers import JsonOutputParser
+from langchain.output_parsers import OutputFixingParser
 from utils.model_loader import ModelLoader
-from langchain_core.output_parsers import JsonOutputParser  
-from langchain.output_parsers import OutputFixingParser 
-
+from logger.custom_logger import CustomLogger
+from exception.custom_exception import DocumentPortalException
+from prompt.prompt_library import PROMPT_REGISTRY   #type:ignore
+from model.models import SummaryResponse,PromptType
 
 class DocumentComparatorLLM:
     def __init__(self):
@@ -17,57 +16,30 @@ class DocumentComparatorLLM:
         self.loader = ModelLoader()
         self.llm = self.loader.load_llm()
         self.parser = JsonOutputParser(pydantic_object=SummaryResponse)
-        self.fixing_parser = OutputFixingParser.from_llm(parser = self.parser, llm = self.llm)      # for fixing maalformed json usign llms
-        self.prompt = PROMPT_REGISTRY["document_comparison"]
+        self.fixing_parser = OutputFixingParser.from_llm(parser=self.parser, llm=self.llm)
+        self.prompt = PROMPT_REGISTRY[PromptType.DOCUMENT_COMPARISON.value]
         self.chain = self.prompt | self.llm | self.parser
-        self.log.info("DocumentComparatorLLM initialized with model and parser.")
-    
-    def compare_documents(self, combined_docs):
-        """
-        Compares two docs and then returns a structured response
-        """
+        self.log.info("DocumentComparatorLLM initialized", model=self.llm)
+
+    def compare_documents(self, combined_docs: str) -> pd.DataFrame:
         try:
             inputs = {
                 "combined_docs": combined_docs,
                 "format_instruction": self.parser.get_format_instructions()
             }
-            self.log.info("Starting document comparison", input = inputs)
+
+            self.log.info("Invoking document comparison LLM chain")
             response = self.chain.invoke(inputs)
-            self.log.info("Document comparison completed", response = response)
+            self.log.info("Chain invoked successfully", response_preview=str(response)[:200])
             return self._format_response(response)
-        
-
         except Exception as e:
-            self.log.error("Error during document comparison", error=str(e))
-            raise DocumentPortalException("Error during document comparison", sys) from e
+            self.log.error("Error in compare_documents", error=str(e))
+            raise DocumentPortalException("Error comparing documents", sys)
 
-    def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame:
-        """
-        Format the response from the LLM into a structured format.
-        """
+    def _format_response(self, response_parsed: list[dict]) -> pd.DataFrame: #type: ignore
         try:
             df = pd.DataFrame(response_parsed)
-            self.log.info("Response formatted into DataFrame", dataframe = df)
             return df
         except Exception as e:
-            self.log.error("Error formatting response into DataFrame", error = str(e))
-            raise DocumentPortalException("Error formatting response", sys)
-    def combine_documents(self) -> str:
-        try:
-            content_dict = {}
-            doc_parts = []
-
-            for filename in sorted(self.base_dir.iterdir()):
-                if filename.is_file() and filename.suffix == ".pdf":
-                    content_dict[filename.name] = self.read_pdf(filename)
-
-            for filename, content in content_dict.items():
-                doc_parts.append(f"Document: {filename}\n{content}")
-
-            combined_text = "\n\n".join(doc_parts)
-            self.log.info("Documents combined", count = len(doc_parts))
-            return combined_text
-
-
-        except Exception as e:
-            self.log.error(f"Error combining docs")
+            self.log.error("Error formatting response into DataFrame", error=str(e))
+            DocumentPortalException("Error formatting response", sys)
