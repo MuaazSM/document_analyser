6e8a1104874e15ca8706297a2671ba4021653211
Muaaz Shaikh <muaaz5731@gmail.com>
2025-09-05 21:23:12 +0530

    Logging error fixed

--- src/singledocchat/data_ingestion.py
+++ src/singledocchat/data_ingestion.py
@@ -12,7 +12,7 @@ from datetime import datetime, timezone
 class SingleDocumentIngestor:
     def __init__(self, data_dir:str = "data/single_document_chat", faiss_dir: str = "faiss_index"):
         try:
-            self = CustomLogger().get_logger(__name__)
+            self.log = CustomLogger().get_logger(__name__)
             self.data_dir = Path(data_dir)
             self.data_dir.mkdir(parents=True, exist_ok=True)
 
